0a474537b35aefba07d1457fb87908ff7501df28
minipeach <2173459+minipeach@users.noreply.github.com>
2025-01-23 17:03:23 +0800

    Create resign.sh

---
+++ resign.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# ipa文件，entitlements.plist，embedded.mobileprovision 请放同一目录下
+
+if ! ([ -f "$1" ]); then
+  echo "\"${1}\" 文件不存在"
+  exit 1
+fi
+
+ipaName=${1%.ipa}
+if [ "$ipaName" = "$1" ]; then
+  echo "\"${1}\" 不是 ipa 文件"
+  exit 1
+fi
+
+
+cert="Apple Development: XXX XXX (9HXXXXXXXX)"
+
+ipaFilePath=$(realpath "$1")  
+echo "IPA 文件路径是：$ipaFilePath"
+
+path=$(dirname "$ipaFilePath")
+echo "IPA 文件目录是：$path"
+
+ipaFileName=$(basename "$ipaFilePath" .ipa)
+echo "IPA 文件名称是：$ipaFileName"
+
+## Step 1: 解压 IPA 文件
+unzip -q -o $ipaFilePath -d $path
+
+appDir=$(ls -d ${path}/Payload/*.app | head -n 1)
+
+
+# 判断是否找到了 .app 文件夹
+if [ -n "$appDir" ]; then
+  echo "appDir 目录是：$appDir"
+else
+  echo "没有找到 .app 目录"
+  exit 1
+fi
+
+
+## Step 2: 移除旧的签名
+rm -rf ${appDir}/_CodeSignature/
+rm -rf ${appDir}/CodeResources
+
+## Step 3: 复制新的 Provisioning Profile
+cp ${path}/embedded.mobileprovision ${appDir}/
+
+## Step 4: 对 Framework 和动态库进行签名
+echo "签名 Frameworks..."
+
+find "$appDir" -type d -name "*.framework" | while read -r framework; do
+  echo "Found framework: $framework"
+  /usr/bin/codesign -f -s "${cert}" --entitlements ${path}/entitlements.plist "$framework"
+  if [ $? -ne 0 ]; then
+    echo "签名失败：$framework"
+    exit 1
+  fi
+done
+
+echo "签名 dylib..."
+
+find "$appDir" -type f -name "*.dylib" | while read -r dylib; do
+  echo "Found dylib: $dylib"
+  /usr/bin/codesign -f -s "${cert}" --entitlements ${path}/entitlements.plist "$dylib"
+  if [ $? -ne 0 ]; then
+    echo "签名失败：$dylib"
+    exit 1
+  fi
+done
+
+
+# Step 5: 对主应用进行签名
+echo "签名主应用..."
+/usr/bin/codesign -f -s "${cert}" --entitlements ${path}/entitlements.plist ${appDir}
+if [ $? -ne 0 ]; then
+  echo "主应用签名失败"
+  exit 1
+fi
+
+
+# Step 6: 打包新的 IPA
+#此处必须cd进去执行zip，否则无法安装
+cd $path
+zip -q -r ${path}/${ipaFileName}New.ipa Payload/
+if [ $? -eq 0 ]; then
+  echo "重新签名的 IPA 已成功打包：${ipaFileName}New.ipa"
+  rm -rf ${path}/Payload/
+else
+  echo "打包失败"
+  exit 1
+fi
